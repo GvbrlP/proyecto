57e3f1dda067d0961d6486d64969f2bc908d77a4
pv-alumno <gab.pizarros@duocuc.cl>
2026-09-03 22:09:08 -0400

    JAVA-007 - Agregar entrada de datos con Scanner

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-        Estudiante estudiante = new Estudiante("ana","infromatica",20);
 
-        estudiante.mostrarInformacion();
 
 
         System.out.println("ingres nombre: ");
@@ -18,9 +16,9 @@ public class Main {
         System.out.println("ingrese edad: ");
         int edad = scanner.nextInt();
 
+        Estudiante estudiante = new Estudiante (nombre, carrera, edad);
 
-
-
+        estudiante.mostrarInformacion();
 
     }
 }
